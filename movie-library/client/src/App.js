20997f0f6339c21ef314bdd843ae382df698c497
Patrick Bradshaw <patrick.j.bradshaw@gmail.com>
2021-08-31 20:06:51 -0700

    Movie: insert/select commands work

--- movie-library/client/src/App.js
+++ movie-library/client/src/App.js
@@ -1,11 +1,90 @@
-import React from 'react';
-import './styles/style.scss'
+import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
+import _ from 'underscore';
+import './styles/style.scss';
 
 function App() {
+  const [moviesList, setMoviesList] = useState([]);
+  const [title, setTitle] = useState('');
+  const [year, setYear] = useState('');
+  const [rating, setRating] = useState('');
+  const [poster, setPoster] = useState(null);
+
+  useEffect(() => {
+    Axios.get('http://localhost:3001/api/get/movies').then(response => {
+      console.log('RESPONSE: ', response.data);
+      setMoviesList(response.data);
+    });
+  }, []);
+
+  const handleSubmit = () => {
+    Axios.post('http://localhost:3001/api/insert/movie', {
+      title,
+      year,
+      rating,
+      poster,
+    }).then(result => {
+      const movie = {
+        id: result.data.newId,
+        title,
+        year,
+        rating,
+        poster,
+      };
+      setMoviesList([...moviesList, { ...movie }]);
+    });
+  };
+
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <main className='bg-dark py-5' style={{ height: '100%' }}>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-6'>
+            <form className='card bg-white card-body' onSubmit={handleSubmit}>
+              <div className='mb-3'>
+                <label>Title</label>
+                <input
+                  type='text'
+                  className='form-control'
+                  onChange={e => setTitle(e.target.value)}
+                />
+              </div>
+              <div className='mb-3'>
+                <label>Year</label>
+                <input
+                  type='text'
+                  className='form-control'
+                  onChange={e => setYear(e.target.value)}
+                />
+              </div>
+              <div className='mb-3'>
+                <label>Rating</label>
+                <input
+                  type='text'
+                  className='form-control'
+                  onChange={e => setRating(e.target.value)}
+                />
+              </div>
+              <button type='submit' className='btn btn-primary'>
+                Submit
+              </button>
+            </form>
+          </div>
+
+          <div className='col-md-6 bg-white'>
+            {moviesList.map(movie => {
+              return (
+                <div key={movie.id}>
+                  <h3>{movie.title}</h3>
+                  <p>{movie.year}</p>
+                  <p>{movie.rating}</p>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
 
---
+++ movie-library/server/index.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 3001;
+var sqlite3 = require('sqlite3').verbose();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// open database
+let db = new sqlite3.Database('./db/movie-library.db', err => {
+  if (err) return console.error(err.message);
+  console.log('Connected to the SQlite database.');
+});
+
+app.get('/api/get/movies', (req, res) => {
+  const sql = 'SELECT * FROM movie';
+  db.all(sql, (err, rows) => {
+    console.log(rows);
+    res.send(rows);
+  });
+});
+
+app.post('/api/insert/movie', (req, res) => {
+  const { title, year, rating, poster } = req.body;
+  const sql =
+    'INSERT INTO movie (title, year, rating, poster) VALUES (?,?,?,?);';
+
+  db.run(sql, [title, year, rating, poster], function (err) {
+    if (err) {
+      res.status(400).send('Unable to save to database.');
+      return;
+    }
+    res.status(200).json({ newId: this.lastID });
+  });
+});
+
+app.delete('/api/delete/movie/:id', (req, res) => {
+  const { id } = req.params;
+  const sql = 'DELETE FROM movie WHERE id = ?';
+
+  db.run(sql, id, (err, result) => {
+    if (err) console.log(err);
+  });
+});
+
+app.put('/api/update/', (req, res) => {
+  const { title, year, rating, poster, id } = req.body;
+  const sql =
+    'UPDATE movie SET title = ?, year = ?, rating = ?, poster = ? WHERE id = ?';
+
+  db.run(sql, [title, year, rating, poster, id], (err, result) => {
+    if (err) console.log(err);
+  });
+});
+
+const server = app.listen(PORT, () => {
+  var host = server.address().address;
+  console.log(`App is up and running. Listening at http://${host}:${PORT}`);
+});
+
+process.on('SIGINT', () => {
+  console.log('CLOSE UP');
+  db.close();
+  server.close();
+});
