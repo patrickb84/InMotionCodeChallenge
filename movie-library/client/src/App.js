a060e02e510f7cec6ab0ca9b86aad42959ea96d0
Patrick Bradshaw <patrick.j.bradshaw@gmail.com>
2021-08-31 20:52:29 -0700

    Movie CRUD operators completed.

--- movie-library/client/src/App.js
+++ movie-library/client/src/App.js
@@ -6,19 +6,20 @@ import './styles/style.scss';
 function App() {
   const [moviesList, setMoviesList] = useState([]);
   const [title, setTitle] = useState('');
+  const [updateTitle, setUpdateTitle] = useState('');
   const [year, setYear] = useState('');
   const [rating, setRating] = useState('');
   const [poster, setPoster] = useState(null);
 
   useEffect(() => {
-    Axios.get('http://localhost:3001/api/get/movies').then(response => {
+    Axios.get('http://localhost:3001/api/movies').then(response => {
       console.log('RESPONSE: ', response.data);
       setMoviesList(response.data);
     });
   }, []);
 
-  const handleSubmit = () => {
-    Axios.post('http://localhost:3001/api/insert/movie', {
+  const handleSubmitMovie = () => {
+    Axios.post('http://localhost:3001/api/movie', {
       title,
       year,
       rating,
@@ -35,12 +36,39 @@ function App() {
     });
   };
 
+  const handleUpdateMovie = id => {
+    const movieRef = _.findWhere(moviesList, { id });
+    console.log(movieRef);
+
+    const movieUpdate = {
+      title: updateTitle ?? movieRef.title,
+      year: movieRef.year,
+      rating: movieRef.rating,
+      poster: movieRef.poster,
+      id,
+    };
+
+    Axios.put('http://localhost:3001/api/movie', {
+      ...movieUpdate,
+    });
+
+    _.extend(_.findWhere(moviesList, { id }), { ...movieUpdate });
+    setMoviesList([...moviesList]);
+  };
+
+  const handleDeleteMovie = id => {
+    Axios.delete(`http://localhost:3001/api/movie/${id}`);
+    setMoviesList(moviesList.filter(movie => movie.id === id));
+  };
+
   return (
     <main className='bg-dark py-5' style={{ height: '100%' }}>
       <div className='container'>
         <div className='row'>
           <div className='col-md-6'>
-            <form className='card bg-white card-body' onSubmit={handleSubmit}>
+            <form
+              className='card bg-white card-body'
+              onSubmit={handleSubmitMovie}>
               <div className='mb-3'>
                 <label>Title</label>
                 <input
@@ -71,13 +99,29 @@ function App() {
             </form>
           </div>
 
-          <div className='col-md-6 bg-white'>
+          <div className='col-md-6'>
             {moviesList.map(movie => {
               return (
-                <div key={movie.id}>
+                <div className='card card-body bg-white mb-3' key={movie.id}>
                   <h3>{movie.title}</h3>
                   <p>{movie.year}</p>
                   <p>{movie.rating}</p>
+                  <div>
+                    <input
+                      type='text'
+                      onChange={e => setUpdateTitle(e.target.value)}
+                    />
+                  </div>
+                  <button
+                    className='btn btn-primary'
+                    onClick={() => handleUpdateMovie(movie.id)}>
+                    Update
+                  </button>
+                  <button
+                    className='btn btn-danger'
+                    onClick={() => handleDeleteMovie(movie.id)}>
+                    Delete
+                  </button>
                 </div>
               );
             })}
